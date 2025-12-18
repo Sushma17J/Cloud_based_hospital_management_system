8a3b398877d4dc79e564f92aacbeff8459bbe687
Sushma17J <joshisushma266@gmail.com>
2025-12-18 07:28:31 +0530

    Delete health_records_django_firebase_v4_full/health_records_django_firebase_v2 directory

--- health_records_django_firebase_v4_full/health_records_django_firebase_v2/asgi.py
+++
@@ -1,4 +0,0 @@
-import os
-from django.core.asgi import get_asgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'health_records_django_firebase_v2.settings')
-application = get_asgi_application()
